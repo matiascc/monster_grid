fbfafcee51f3a078847b4b107e58388bcbbf6c24
Matias Caporale <matiasccyo@gmail.com>
2020-07-23 13:27:48 -0300

    Updates

---
+++ precache-manifest.fdc5303ff586f90210dbcbc2b060ff81.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "121101eb0a3d117b53f26db2bbf1e8d5",
+    "url": "/monster_grid/index.html"
+  },
+  {
+    "revision": "549a8436251258f7b5e1",
+    "url": "/monster_grid/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "2094a85f60119b5fa37d",
+    "url": "/monster_grid/static/css/main.ba676505.chunk.css"
+  },
+  {
+    "revision": "549a8436251258f7b5e1",
+    "url": "/monster_grid/static/js/2.3cc0d141.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster_grid/static/js/2.3cc0d141.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2094a85f60119b5fa37d",
+    "url": "/monster_grid/static/js/main.ef186ddd.chunk.js"
+  },
+  {
+    "revision": "4adaa87164c541df0d9c",
+    "url": "/monster_grid/static/js/runtime-main.aaebda9b.js"
+  },
+  {
+    "revision": "12622ada44bf26aecf5513f716116199",
+    "url": "/monster_grid/static/media/disney_font.12622ada.woff"
+  }
+]);
